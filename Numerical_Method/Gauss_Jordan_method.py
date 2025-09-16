0e98949aa9209e914e6ebedb9d83e5b335028193
Rajesh-Biswas <rajesh18@cse.pstu.ac.bd>
2025-09-17 02:45:43 +0600

    17-09-2025

--- Numerical_Method/Gauss_Jordan_method.py
+++ Numerical_Method/Gauss_Jordan_method.py
@@ -1,20 +1,30 @@
-import numpy as np
 
-def gauss_jordan(a, b):
-    aug = np.hstack((a.astype(float), b.reshape(-1, 1).astype(float)))
-    n = len(b)
+def gauss_jordan(A):
     
-    for i in range(n):
-        aug[i] = aug[i] / aug[i, i]
-        for j in range(n):
-            if i != j:
-                aug[j] = aug[j] - aug[j, i] * aug[i]
-    return aug[:, -1]
+    fac1=A[1][0]/A[0][0]
+    fac2=A[2][0]/A[0][0]
+    for i in range(4):
+       A[1][i]=A[1][i]-A[0][i]*fac1
+       A[2][i]=A[2][i]-A[0][i]*fac2
+       
+    fac1=A[0][1]/A[1][1]
+    fac2=A[2][1]/A[1][1]
+    for i in range(4):
+       A[0][i]=A[0][i]-A[1][i]*fac1
+       A[2][i]=A[2][i]-A[1][i]*fac2
+    
+    fac1=A[1][2]/A[2][2]
+    fac2=A[0][2]/A[2][2]
+    for i in range(4):
+       A[1][i]=A[1][i]-A[2][i]*fac1
+       A[0][i]=A[0][i]-A[2][i]*fac2
+    
+   
+    return [A[0][3]/A[0][0],A[1][3]/A[1][1],A[2][3]/A[2][2]]
 
-A = np.array([[2, 1, -1],
-              [-3, -1, 2],
-              [-2, 1, 2]])
-B = np.array([8, -11, -3])
+A = [[2.0, 1.0, -1.0, 8.0],
+     [-3.0, -1.0, 2.0,-11.0],
+     [-2.0, 1.0, 2.0, -3.0]]
 
-solution = gauss_jordan(A, B)
-print("Solution: x = {:.2f}, y = {:.2f}, z = {:.2f}".format(*solution))
+solution = gauss_jordan(A)
+print("Solution:", solution)
